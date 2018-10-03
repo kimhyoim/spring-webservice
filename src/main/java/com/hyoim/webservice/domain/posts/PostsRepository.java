76dee336de1f37b1509266d16aa3d3801e9ba5b7
kimhyoim <kimhyoim23@gmail.com>
2018-10-04 04:14:31 +0900

    SpringBoot & JPA로 간단 API 만들기

---
+++ src/main/java/com/hyoim/webservice/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.hyoim.webservice.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
--- src/main/java/com/hyoim/webservice/web/WebRestController.java
+++ src/main/java/com/hyoim/webservice/web/WebRestController.java
@@ -1,14 +1,28 @@
 package com.hyoim.webservice.web;
 
+import com.hyoim.webservice.domain.posts.PostsRepository;
+import com.hyoim.webservice.dto.posts.PostsSaveRequestDto;
+
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 
 }
